3cb958f8d20acfa4dc29cc7c1d8493cbe1e0df96
Arnaud Lachaume <arnaud.lachaume@maestrano.com>
2014-10-14 16:42:57 +1100

    added project structure

---
+++ src/main/java/com/maestrano/App.java
@@ -0,0 +1,13 @@
+package com.maestrano;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
