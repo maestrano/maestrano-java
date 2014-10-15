e18e0ea7865409c9f189dc4ce7ffb6a228fcd85c
Arnaud Lachaume <arnaud.lachaume@maestrano.com>
2014-10-15 14:55:32 +1100

    added Certificate class

--- src/main/java/com/maestrano/saml/Certificate.java
+++ src/main/java/com/maestrano/saml/Certificate.java
@@ -1,39 +1,42 @@
-// package com.maestrano.saml;
-//
-// import java.io.ByteArrayInputStream;
-// import java.security.cert.CertificateException;
-// import java.security.cert.CertificateFactory;
-// import java.security.cert.X509Certificate;
-// import org.apache.commons.codec.binary.Base64;
-//
-// public class Certificate {
-//
-//   private X509Certificate x509Cert;
-//
-//   /**
-//    * Loads certificate from a base64 encoded string
-//    */
-//    public void loadCertificate(String certificate) throws CertificateException {
-//     CertificateFactory fty = CertificateFactory.getInstance("X.509");
-//     ByteArrayInputStream bais = new ByteArrayInputStream(Base64.decodeBase64(certificate.getBytes()));
-//
-//     x509Cert = (X509Certificate)fty.generateCertificate(bais);
-//   }
-//
-//   /**
-//    * Loads a certificate from a encoded base64 byte array.
-//    * @param certificate an encoded base64 byte array.
-//    * @throws CertificateException In case it can't load the certificate.
-//    */
-//   public void loadCertificate(byte[] certificate) throws CertificateException {
-//     CertificateFactory fty = CertificateFactory.getInstance("X.509");
-//     ByteArrayInputStream bais = new ByteArrayInputStream(Base64.decodeBase64(certificate));
-//
-//     x509Cert = (X509Certificate)fty.generateCertificate(bais);
-//
-//   }
-//
-//   public X509Certificate getX509Cert() {
-//     return x509Cert;
-//   }
-// }
+package com.maestrano.saml;
+
+import java.io.ByteArrayInputStream;
+import java.io.UnsupportedEncodingException;
+import java.security.cert.CertificateException;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
+
+import javax.xml.bind.DatatypeConverter;
+
+public class Certificate {
+
+  private X509Certificate x509Cert;
+
+  /**
+   * Loads certificate from a base64 encoded string
+   */
+   public void loadCertificate(String certificate) throws CertificateException {
+    CertificateFactory fty = CertificateFactory.getInstance("X.509");
+    
+    ByteArrayInputStream bais = new ByteArrayInputStream(DatatypeConverter.parseBase64Binary(certificate));
+
+    x509Cert = (X509Certificate)fty.generateCertificate(bais);
+  }
+
+  /**
+   * Loads a certificate from a encoded base64 byte array.
+   * @param certificate an encoded base64 byte array.
+   * @throws CertificateException In case it can't load the certificate.
+ * @throws UnsupportedEncodingException 
+   */
+  public void loadCertificate(byte[] certificate) throws CertificateException, UnsupportedEncodingException {
+    String certAsString = new String(certificate, "UTF-8");
+    
+    this.loadCertificate(certAsString);
+
+  }
+
+  public X509Certificate getX509Cert() {
+    return x509Cert;
+  }
+}
--- src/test/java/com/maestrano/saml/CertificateTest.java
+++ src/test/java/com/maestrano/saml/CertificateTest.java
@@ -1,8 +1,54 @@
 package com.maestrano.saml;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import java.io.UnsupportedEncodingException;
+import java.security.cert.X509Certificate;
+import java.security.cert.CertificateException;
+
+import javax.xml.bind.DatatypeConverter;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.maestrano.saml.Certificate;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+public class CertificateTest {
+  private static Certificate certificate;
+  private static String certStr;
+  
+  @BeforeClass
+  public static void beforeAll() {
+	  certStr = "MIIDezCCAuSgAwIBAgIJAOehBr+YIrhjMA0GCSqGSIb3DQEBBQUAMIGGMQswCQYDVQQGEwJBVTEMMAoGA1UECBMDTlNXMQ8wDQYDVQQHEwZTeWRuZXkxGjAYBgNVBAoTEU1hZXN0cmFubyBQdHkgTHRkMRYwFAYDVQQDEw1tYWVzdHJhbm8uY29tMSQwIgYJKoZIhvcNAQkBFhVzdXBwb3J0QG1hZXN0cmFuby5jb20wHhcNMTQwMTA0MDUyMjM5WhcNMzMxMjMwMDUyMjM5WjCBhjELMAkGA1UEBhMCQVUxDDAKBgNVBAgTA05TVzEPMA0GA1UEBxMGU3lkbmV5MRowGAYDVQQKExFNYWVzdHJhbm8gUHR5IEx0ZDEWMBQGA1UEAxMNbWFlc3RyYW5vLmNvbTEkMCIGCSqGSIb3DQEJARYVc3VwcG9ydEBtYWVzdHJhbm8uY29tMIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDVkIqo5t5PafluP2zbSbzxn29n6HxKnTcsubycLBEs0jkTkdG7seF1LPqnXl8jFM9NGPiBFkiaR15I5w482IW6mC7s8T2CbZEL3qqQEAzztEPnxQg0twswyIZWNyuHYzf9fw0AnohBhGu228EZWaezzT2F333FOVGSsTn1+u6tFwIDAQABo4HuMIHrMB0GA1UdDgQWBBSvrNxoeHDm9nhKnkdpe0lZjYD1GzCBuwYDVR0jBIGzMIGwgBSvrNxoeHDm9nhKnkdpe0lZjYD1G6GBjKSBiTCBhjELMAkGA1UEBhMCQVUxDDAKBgNVBAgTA05TVzEPMA0GA1UEBxMGU3lkbmV5MRowGAYDVQQKExFNYWVzdHJhbm8gUHR5IEx0ZDEWMBQGA1UEAxMNbWFlc3RyYW5vLmNvbTEkMCIGCSqGSIb3DQEJARYVc3VwcG9ydEBtYWVzdHJhbm8uY29tggkA56EGv5giuGMwDAYDVR0TBAUwAwEB/zANBgkqhkiG9w0BAQUFAAOBgQCcMPgV0CpumKRMulOeZwdpnyLQI/NTr3VVHhDDxxCzcB0zlZ2xyDACGnIG2cQJJxfc2GcsFnb0BMw48K6TEhAaV92Q7bt1/TYRvprvhxUNMX2N8PHaYELFG2nWfQ4vqxESRkjkjqy+H7vir/MOF3rlFjiv5twAbDKYHXDT7v1YCg==";
+  }
+
+  @Before
+  public void beforeEach() {
+    certificate = new Certificate();
+  }
+
+  @Test
+  public void itLoadsACertificateFromString() throws CertificateException
+  {
+	  certificate.loadCertificate(certStr);
+	  assertEquals(certStr,DatatypeConverter.printBase64Binary(certificate.getX509Cert().getEncoded()));
+  }
+
+  @Test
+  public void itLoadsACertificateFromByteArray() throws CertificateException, UnsupportedEncodingException
+  {
+	  certificate.loadCertificate(certStr.getBytes());
+	  assertEquals(certStr,DatatypeConverter.printBase64Binary(certificate.getX509Cert().getEncoded()));
+  }
+    
+    
+}
 
 
 
