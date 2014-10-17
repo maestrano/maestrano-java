e483a77df22f296a982f00abdb6a6eb988930ea1
Arnaud Lachaume <arnaud.lachaume@maestrano.com>
2014-10-17 17:07:53 +1100

    added Saml Request class

--- src/main/java/com/maestrano/saml/Request.java
+++ src/main/java/com/maestrano/saml/Request.java
@@ -1,102 +1,143 @@
-//package com.maestrano.saml;
-//
-//import java.io.ByteArrayOutputStream;
-//import java.nio.charset.Charset;
-//import java.text.SimpleDateFormat;
-//import java.util.Date;
-//import java.util.Map;
-//import java.util.UUID;
-//
-//import javax.xml.stream.XMLOutputFactory;
-//import javax.xml.stream.XMLStreamException;
-//import javax.xml.stream.XMLStreamWriter;
-//
-//import org.apache.commons.codec.binary.Base64;
-//
-//import com.maestrano.lib.onelogin.AccountSettings;
-//import com.maestrano.lib.onelogin.AppSettings;
-//
-//public class Request {
-//	
-//	private String id;
-//	private String issueInstant;
-//	//private AppSettings appSettings;
-//	
-//	public Request(Map<String, String> parameters)
-//	{		
-//		this.appSettings = appSettings;
-//		id="_"+UUID.randomUUID().toString();		
-//		SimpleDateFormat simpleDf = new SimpleDateFormat("yyyy-MM-dd'T'H:mm:ss");
-//		issueInstant = simpleDf.format(new Date());		
-//	}
-//	
-//	public String getXmlBase64Request() throws XMLStreamException {
-//		ByteArrayOutputStream baos = new ByteArrayOutputStream();		
-//		XMLOutputFactory factory = XMLOutputFactory.newInstance();
-//		XMLStreamWriter writer = factory.createXMLStreamWriter(baos);
-//		
-//		// Prepare XML request
-//		writer.writeStartElement("samlp", "AuthnRequest", "urn:oasis:names:tc:SAML:2.0:protocol");
-//		writer.writeNamespace("samlp","urn:oasis:names:tc:SAML:2.0:protocol");
-//		
-//		writer.writeAttribute("ID", id);
-//		writer.writeAttribute("Version", "2.0");
-//		writer.writeAttribute("IssueInstant", this.issueInstant);
-//		writer.writeAttribute("ProtocolBinding", "urn:oasis:names:tc:SAML:2.0:bindings:HTTP-POST");
-//		writer.writeAttribute("AssertionConsumerServiceURL", this.appSettings.getAssertionConsumerServiceUrl());
-//		
-//		writer.writeStartElement("saml","Issuer","urn:oasis:names:tc:SAML:2.0:assertion");
-//		writer.writeNamespace("saml","urn:oasis:names:tc:SAML:2.0:assertion");
-//		writer.writeCharacters(this.appSettings.getIssuer());
-//		writer.writeEndElement();
-//		
-//		writer.writeStartElement("samlp", "NameIDPolicy", "urn:oasis:names:tc:SAML:2.0:protocol");
-//		
-//		writer.writeAttribute("Format", "urn:oasis:names:tc:SAML:2.0:nameid-format:unspecified");
-//		writer.writeAttribute("AllowCreate", "true");
-//		writer.writeEndElement();
-//		
-//		writer.writeStartElement("samlp","RequestedAuthnContext","urn:oasis:names:tc:SAML:2.0:protocol");
-//		
-//		writer.writeAttribute("Comparison", "exact");
-//		writer.writeEndElement();
-//		
-//		writer.writeStartElement("saml","AuthnContextClassRef","urn:oasis:names:tc:SAML:2.0:assertion");
-//		writer.writeNamespace("saml", "urn:oasis:names:tc:SAML:2.0:assertion");
-//		writer.writeCharacters("urn:oasis:names:tc:SAML:2.0:ac:classes:PasswordProtectedTransport");
-//		writer.writeEndElement();
-//		
-//		writer.writeEndElement();
-//		writer.flush();		
-//		
-//		// Base64 encode the result
-//		byte [] encoded = Base64.encodeBase64Chunked(baos.toByteArray());
-//		String result = new String(encoded,Charset.forName("UTF-8"));
-//						
-//		return result;
-//	}
-//	
-// 	public static String getRidOfCRLF(String what) {
-//		String lf = "%0D";
-//		String cr = "%0A";
-//		String now = lf;
-//
-//		int index = what.indexOf(now);
-//		StringBuffer r = new StringBuffer();
-//
-//		while (index!=-1) {
-//			r.append(what.substring(0,index));
-//			what = what.substring(index+3,what.length());
-//			
-//			if (now.equals(lf)) {
-//				now = cr;
-//			} else {
-//				now = lf;
-//			}
-//			
-//			index = what.indexOf(now);
-//		}
-//		return r.toString();
-//	}		
-//
-//}
+package com.maestrano.saml;
+
+import java.io.ByteArrayOutputStream;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Map;
+import java.util.UUID;
+
+import javax.xml.stream.XMLOutputFactory;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamWriter;
+import javax.xml.bind.DatatypeConverter;
+
+import com.maestrano.Maestrano;
+
+public class Request {
+	
+	private String id;
+	private String issueInstant;
+	private Settings settings;
+	private Map<String,String> parameters;
+	
+	/**
+	 * Constructor
+	 */
+	public Request() {
+		this.settings = Maestrano.ssoService().getSamlSettings();
+		id = "_" + UUID.randomUUID().toString();
+		SimpleDateFormat simpleDf = new SimpleDateFormat("yyyy-MM-dd'T'H:mm:ssZ");
+		issueInstant = simpleDf.format(new Date());	
+	}
+	
+	/**
+	 * Constructor
+	 * @param Map<String,String> request parameters
+	 */
+	public Request(Map<String, String> parameters)
+	{	
+		this();
+		this.parameters = parameters;
+	}
+	
+	/**
+	 * 
+	 * @return String base 64 encoded SAML request
+	 * @throws XMLStreamException
+	 */
+	public String getXmlBase64Request() throws XMLStreamException {
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();		
+		XMLOutputFactory factory = XMLOutputFactory.newInstance();
+		XMLStreamWriter writer = factory.createXMLStreamWriter(baos);
+		
+		// Prepare XML request
+		writer.writeStartElement("samlp", "AuthnRequest", "urn:oasis:names:tc:SAML:2.0:protocol");
+			writer.writeNamespace("samlp","urn:oasis:names:tc:SAML:2.0:protocol");
+			writer.writeAttribute("ID", id);
+			writer.writeAttribute("Version", "2.0");
+			writer.writeAttribute("IssueInstant", this.issueInstant);
+			writer.writeAttribute("ProtocolBinding", "urn:oasis:names:tc:SAML:2.0:bindings:HTTP-POST");
+			writer.writeAttribute("AssertionConsumerServiceURL", this.settings.getAssertionConsumerServiceUrl());
+		
+			writer.writeStartElement("saml","Issuer","urn:oasis:names:tc:SAML:2.0:assertion");
+				writer.writeNamespace("saml","urn:oasis:names:tc:SAML:2.0:assertion");
+				writer.writeCharacters(this.settings.getIssuer());
+			writer.writeEndElement();
+		
+			writer.writeStartElement("samlp", "NameIDPolicy", "urn:oasis:names:tc:SAML:2.0:protocol");
+				writer.writeAttribute("Format", "urn:oasis:names:tc:SAML:2.0:nameid-format:unspecified");
+				writer.writeAttribute("AllowCreate", "true");
+			writer.writeEndElement();
+		
+			writer.writeStartElement("samlp","RequestedAuthnContext","urn:oasis:names:tc:SAML:2.0:protocol");
+				writer.writeAttribute("Comparison", "exact");
+			writer.writeEndElement();
+		
+			writer.writeStartElement("saml","AuthnContextClassRef","urn:oasis:names:tc:SAML:2.0:assertion");
+				writer.writeNamespace("saml", "urn:oasis:names:tc:SAML:2.0:assertion");
+				writer.writeCharacters("urn:oasis:names:tc:SAML:2.0:ac:classes:PasswordProtectedTransport");
+			writer.writeEndElement();
+		writer.writeEndElement();
+		writer.flush();		
+		
+		// Base64 encode the result
+		String result = DatatypeConverter.printBase64Binary(baos.toByteArray());
+		
+		return result;
+	}
+	
+	/**
+	 * Return the full IDP redirect url with encoded SAML request
+	 * @return String SAML request url
+	 * @throws XMLStreamException 
+	 * @throws UnsupportedEncodingException 
+	 */
+	public String getRedirectUrl() throws UnsupportedEncodingException, XMLStreamException {
+		String url = this.settings.getIdpSsoTargetUrl();
+		url += "?SAMLRequest=";
+		url += this.getRidOfCRLF(URLEncoder.encode(this.getXmlBase64Request(),"UTF-8"));
+		
+		if (this.parameters != null) {
+			for (Map.Entry<String, String> param : this.parameters.entrySet())
+			{
+				String key = this.getRidOfCRLF(URLEncoder.encode(param.getKey(),"UTF-8"));
+				String val = this.getRidOfCRLF(URLEncoder.encode(param.getValue(), "UTF-8"));
+				url += "&" + key + "=" + val;
+			}
+		}
+		
+		
+		return url;
+	}
+	
+	/**
+	 * Clean a string from CRLF characters
+	 * @param String string to be cleaned
+	 * @return String clean string
+	 */
+ 	private String getRidOfCRLF(String origStr) {
+		String lf = "%0D";
+		String cr = "%0A";
+		String now = lf;
+
+		int index = origStr.indexOf(now);
+		StringBuffer r = new StringBuffer();
+
+		while (index!=-1) {
+			r.append(origStr.substring(0,index));
+			origStr = origStr.substring(index+3,origStr.length());
+			
+			if (now.equals(lf)) {
+				now = cr;
+			} else {
+				now = lf;
+			}
+			
+			index = origStr.indexOf(now);
+		}
+		return r.toString();
+	}		
+
+}
---
+++ src/test/java/com/maestrano/saml/RequestTest.java
@@ -0,0 +1,59 @@
+package com.maestrano.saml;
+
+import java.io.IOException;
+import java.io.StringReader;
+import java.io.UnsupportedEncodingException;
+import java.util.Properties;
+
+import javax.xml.bind.DatatypeConverter;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.stream.XMLStreamException;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.w3c.dom.Document;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+
+import com.maestrano.Maestrano;
+
+import static org.junit.Assert.assertEquals;
+
+public class RequestTest {
+	private Properties props = new Properties();
+	private Request subject;
+	
+	@Before
+	public void beforeEach() {
+		props.setProperty("app.environment", "production");
+		props.setProperty("app.host", "https://mysuperapp.com");
+		props.setProperty("api.id", "someid");
+		props.setProperty("api.key", "somekey");
+		Maestrano.configure(props);
+		subject = new Request();
+	}
+	
+	@Test
+	public void getXmlBase64Request_itReturnsABase64EncodedRequest() throws XMLStreamException {
+		DatatypeConverter.parseBase64Binary(subject.getXmlBase64Request());
+	}
+	
+	@Test
+	public void getXmlBase64Request_itSetsTheRightSAMLParameters() throws XMLStreamException, ParserConfigurationException, SAXException, IOException {
+		String actual;
+		byte[] encoded = DatatypeConverter.parseBase64Binary(subject.getXmlBase64Request());
+		String xmlReq = new String(encoded,"UTF-8");
+		
+		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+		DocumentBuilder db = dbf.newDocumentBuilder();
+		Document dom = db.parse(new InputSource(new StringReader(xmlReq)));
+		
+		actual = dom.getElementsByTagName("saml:Issuer").item(0).getFirstChild().getTextContent();
+		assertEquals(props.getProperty("api.id"), actual);
+		
+		actual = dom.getElementsByTagName("samlp:AuthnRequest").item(0).getAttributes().getNamedItem("AssertionConsumerServiceURL").getNodeValue();
+		assertEquals(props.getProperty("app.host") + "/maestrano/auth/saml/consume.jsp", actual);
+	}
+}
