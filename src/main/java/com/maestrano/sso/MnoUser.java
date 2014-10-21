d73d107e8194b5bcf09f08880f367324966ef4d7
Arnaud Lachaume <arnaud.lachaume@maestrano.com>
2014-10-21 16:07:00 +1100

    implemented MnoUser model

--- src/main/java/com/maestrano/sso/MnoUser.java
+++ src/main/java/com/maestrano/sso/MnoUser.java
@@ -1,5 +1,176 @@
 package com.maestrano.sso;
 
-public class MnoUser {
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Map;
 
+import com.maestrano.Maestrano;
+
+public class MnoUser {
+	
+	public String ssoSession;
+    public Date ssoSessionRecheck;
+    public String groupUid;
+    public String groupRole;
+    public String uid;
+    public String virtualUid;
+    public String email;
+    public String virtualEmail;
+    public String firstName;
+    public String lastName;
+    public String country;
+    public String companyName;
+	
+	/**
+	 * Constructor
+	 * @param samlResp a SAML Response from Maestrano IDP
+	 * @throws ParseException 
+	 */
+	public MnoUser(com.maestrano.saml.Response samlResponse) throws ParseException {
+		SimpleDateFormat simpleDf = new SimpleDateFormat("yyyy-MM-dd'T'H:mm:ssZ");
+		Map<String,String> att = samlResponse.getAttributes();
+        
+		this.ssoSession = att.get("mno_session");
+        this.ssoSessionRecheck = simpleDf.parse(att.get("mno_session_recheck"));
+        this.groupUid = att.get("group_uid");
+        this.groupRole = att.get("group_role");
+        this.uid = att.get("uid");
+        this.virtualUid = att.get("virtual_uid");
+        this.email = att.get("email");
+        this.virtualEmail = att.get("virtual_email");
+        this.firstName = att.get("name");
+        this.lastName = att.get("surname");
+        this.country = att.get("country");
+        this.companyName = att.get("company_name");
+	}
+	
+	/**
+	 * Return the real UID if Maestrano Sso Creation Mode is set
+     * to "real" and the Virtual UID otherwise ("virtual" mode)
+	 * @return String uid to use in application
+	 */
+    public String toUid()
+    {
+    	if (Maestrano.ssoService().getCreationMode().equals("real")) {
+    		return this.uid;
+    	} else {
+    		return this.virtualUid;
+    	}
+    }
+    
+    /**
+     * Return the real email if Maestrano Sso Creation Mode is set
+     * to "real" and the Virtual email otherwise ("virtual" mode)
+     * @return
+     */
+    public String toEmail()
+    {
+    	if (Maestrano.ssoService().getCreationMode().equals("real")) {
+    		return this.email;
+    	} else {
+    		return this.virtualEmail;
+    	}
+    }
+	
+	/**
+	 * Return the current user session token
+	 * @return String session token
+	 */
+	public String getSsoSession() {
+		return ssoSession;
+	}
+	
+	/**
+	 * Return when the user session should be remotely checked
+	 * @return Date session check time
+	 */
+	public Date getSsoSessionRecheck() {
+		return ssoSessionRecheck;
+	}
+	
+	/**
+	 * Return the user group UID 
+	 * @return String group UID
+	 */
+	public String getGroupUid() {
+		return groupUid;
+	}
+	
+	/**
+	 * Return the user role in the group
+	 * Roles are: 'Member', 'Power User', 'Admin', 'Super Admin'
+	 * @return String user role in group
+	 */
+	public String getGroupRole() {
+		return groupRole;
+	}
+	
+	/**
+	 * The Maestrano user UID
+	 * @return String user UID
+	 */
+	public String getUid() {
+		return uid;
+	}
+	
+	/**
+	 * The user virtual UID which is truly unique across users and groups
+	 * @return String user virtual uid
+	 */
+	public String getVirtualUid() {
+		return virtualUid;
+	}
+	
+	/**
+	 * The actual user email 
+	 * @return String user email
+	 */
+	public String getEmail() {
+		return email;
+	}
+	
+	/**
+	 * Virtual email that can be used instead of regular email fields
+	 * This email is unique across users and groups
+	 * All emails sent to this email address are redirected to the real
+	 * user email
+	 * @return String virtual email
+	 */
+	public String getVirtualEmail() {
+		return virtualEmail;
+	}
+	
+	/**
+	 * User first name
+	 * @return String user first name
+	 */
+	public String getFirstName() {
+		return firstName;
+	}
+	
+	/**
+	 * User last name
+	 * @return String user last name
+	 */
+	public String getLastName() {
+		return lastName;
+	}
+	
+	/**
+	 * ALPHA2 code of user country
+	 * @return
+	 */
+	public String getCountry() {
+		return country;
+	}
+	
+	/**
+	 * Company name entered by the user
+	 * Can be empty
+	 * @return String company name
+	 */
+	public String getCompanyName() {
+		return companyName;
+	}
 }
--- src/test/java/com/maestrano/sso/MnoUserTest.java
+++ src/test/java/com/maestrano/sso/MnoUserTest.java
@@ -1,5 +1,92 @@
 package com.maestrano.sso;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Map;
+import java.util.Properties;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+import com.maestrano.Maestrano;
+import com.maestrano.saml.Response;
+import com.maestrano.testhelpers.SamlMnoRespStub;
+
 public class MnoUserTest {
+	private Properties props = new Properties();
+	private Response samlResp;
+	private MnoUser subject;
+
+	@Before
+	public void beforeEach() throws Exception {
+		props.setProperty("app.environment", "production");
+		props.setProperty("app.host", "https://mysuperapp.com");
+		props.setProperty("api.id", "someid");
+		props.setProperty("api.key", "somekey");
+		Maestrano.configure(props);
+
+		samlResp = (Response) new SamlMnoRespStub();
+		subject = new MnoUser(samlResp);
+	}
+
+	@Test
+	public void constructor_itExtractsTheRightAttributesFromTheSamlResponse() throws ParseException
+	{
+		SimpleDateFormat simpleDf = new SimpleDateFormat("yyyy-MM-dd'T'H:mm:ssZ");
+
+		Map<String,String> att = samlResp.getAttributes();
+
+		assertEquals(att.get("mno_session"), subject.getSsoSession());
+		assertEquals(simpleDf.parse(att.get("mno_session_recheck")), subject.getSsoSessionRecheck());
+		assertEquals(att.get("group_uid"), subject.getGroupUid());
+		assertEquals(att.get("group_role"), subject.getGroupRole());
+		assertEquals(att.get("uid"), subject.getUid());
+		assertEquals(att.get("virtual_uid"), subject.getVirtualUid());
+		assertEquals(att.get("email"), subject.getEmail());
+		assertEquals(att.get("virtual_email"), subject.getVirtualEmail());
+		assertEquals(att.get("name"), subject.getFirstName());
+		assertEquals(att.get("surname"), subject.getLastName());
+		assertEquals(att.get("country"), subject.getCountry());
+		assertEquals(att.get("company_name"), subject.getCompanyName());
+	}
+
+	@Test
+	public void toUid_whenReal_itReturnsTheRightUid()
+	{
+		props.setProperty("sso.creationMode", "real");
+		Maestrano.configure(props);
+
+		assertEquals(subject.getUid(),subject.toUid());
+	}
+
+	@Test
+	public void toUid_whenVirtual_itReturnsTheRightUid()
+	{
+		props.setProperty("sso.creationMode", "virtual");
+		Maestrano.configure(props);
+		
+		assertEquals(subject.getVirtualUid(),subject.toUid());
+	}
+
+	@Test
+	public void toEmail_whenReal_itReturnsTheRightEmail()
+	{
+		props.setProperty("sso.creationMode", "real");
+		Maestrano.configure(props);
+
+		assertEquals(subject.getEmail(),subject.toEmail());
+	}
+
+	@Test
+	public void toEmail_whenVirtual_itReturnsTheRightEmail()
+	{
+		props.setProperty("sso.creationMode", "virtual");
+		Maestrano.configure(props);
+
+		assertEquals(subject.getVirtualEmail(),subject.toEmail());
+	}
+
 
 }
