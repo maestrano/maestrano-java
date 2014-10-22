555cab03de3dede44186ce7c38a9628f129c8e93
Arnaud Lachaume <arnaud.lachaume@maestrano.com>
2014-10-22 18:54:07 +1100

    added full range of http methods

--- src/main/java/com/maestrano/net/MnoHttpClient.java
+++ src/main/java/com/maestrano/net/MnoHttpClient.java
@@ -3,6 +3,7 @@ package com.maestrano.net;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.ProtocolException;
@@ -11,10 +12,10 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class MnoHttpClient {
-	private String userAgent;
+	private String defaultUserAgent;
 	
 	public MnoHttpClient() {
-		this.userAgent = "maestrano-java/" + System.getProperty("java.version");
+		this.defaultUserAgent = "maestrano-java/" + System.getProperty("java.version");
 	}
 	
 	/**
@@ -24,7 +25,53 @@ public class MnoHttpClient {
 	 * @throws IOException
 	 */
 	public String get(String url) throws IOException {
-		return performRequest(url,"GET");
+		return get(url,null);
+	}
+	
+	/**
+	 * Perform a GET request on the specified endpoint
+	 * @param url
+	 * @return response body
+	 * @throws IOException
+	 */
+	public String get(String url, Map<String,String> header) throws IOException {
+		return performRequest(url,"GET",header);
+	}
+	
+	/**
+	 * Perform a POST request on the specified endpoint
+	 * @param url
+	 * @param header
+	 * @param payload
+	 * @return response body
+	 * @throws IOException
+	 */
+	public String post(String url, Map<String,String> header, String payload) throws IOException {
+		return performRequest(url,"POST",header,payload);
+	}
+	
+	/**
+	 * Perform a PUT request on the specified endpoint
+	 * @param url
+	 * @param header
+	 * @param payload
+	 * @return response body
+	 * @throws IOException
+	 */
+	public String delete(String url, Map<String,String> header, String payload) throws IOException {
+		return performRequest(url,"DELETE",header,payload);
+	}
+	
+	/**
+	 * Perform a PUT request on the specified endpoint
+	 * @param url
+	 * @param header
+	 * @param payload
+	 * @return response body
+	 * @throws IOException
+	 */
+	public String put(String url, Map<String,String> header, String payload) throws IOException {
+		return performRequest(url,"PUT",header,payload);
 	}
 	
 	/**
@@ -35,13 +82,63 @@ public class MnoHttpClient {
 	 * @throws IOException
 	 */
 	protected String performRequest(String url, String method) throws IOException {
+		return performRequest(url,method,null);
+	}
+	
+	/**
+	 * Perform a request to the remote endpoint
+	 * @param url the remote endpoint to contact
+	 * @param method such as 'GET', 'PUT', 'POST' or 'DELETE'
+	 * @param header values
+	 * @return response body
+	 * @throws IOException
+	 */
+	protected String performRequest(String url, String method, Map<String,String> header) throws IOException {
+		return performRequest(url,method,header,null);
+	}
+	
+	/**
+	 * Perform a request to the remote endpoint
+	 * @param url the remote endpoint to contact
+	 * @param method such as 'GET', 'PUT', 'POST' or 'DELETE'
+	 * @param header values
+	 * @param payload data to send
+	 * @return response body
+	 * @throws IOException
+	 */
+	protected String performRequest(String url, String method, Map<String,String> header, String payload) throws IOException {
 		// Prepare header
-		Map<String,String> header = new HashMap<String,String>();
-		header.put("method",method);
-		header.put("User-Agent",userAgent);
+		if (header == null) {
+			header = new HashMap<String,String>();
+		}
+		
+		// Set method
+		header.put("method",method.toUpperCase());
+		
+		// Set user agent
+		if (header.get("User-Agent") == null || header.get("User-Agent").isEmpty()) {
+			header.put("User-Agent",defaultUserAgent);
+		}
+		
 		
 		// Get connection
 		HttpURLConnection conn = openConnection(url,header);
+		
+		// Send Data if PUT/POST
+		if (payload != null) {
+			if (method.equalsIgnoreCase("PUT") || method.equalsIgnoreCase("POST")) {
+				OutputStream output = null;
+				try {
+					output = conn.getOutputStream();
+					output.write(payload.getBytes());
+				} finally {
+					if (output != null) {
+						output.close();
+					}
+				}
+			}
+		}
+		
 		
 		// Parse response
 		BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
@@ -87,7 +184,7 @@ public class MnoHttpClient {
 			conn.setRequestMethod(header.get("method"));
 			conn.addRequestProperty("User-Agent", header.get("User-Agent"));
 			conn.setInstanceFollowRedirects(true);
-
+			
 			// Check if redirect
 			redirect = false;
 			int status = conn.getResponseCode();
