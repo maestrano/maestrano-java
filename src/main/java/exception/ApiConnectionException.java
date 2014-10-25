4cbdfb11d07febe6ec147050c85774bc49083f47
Arnaud Lachaume <arnaud.lachaume@maestrano.com>
2014-10-25 14:30:12 +1100

    added exception

---
+++ src/main/java/exception/ApiConnectionException.java
@@ -0,0 +1,13 @@
+package exception;
+
+public class ApiConnectionException extends MnoException {
+	private static final long serialVersionUID = 1L;
+	
+	public ApiConnectionException(String message) {
+		super(message);
+	}
+	
+	public ApiConnectionException(String message, Throwable e) {
+		super(message, e);
+	}
+}
