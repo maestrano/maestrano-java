87c31828b50749e17348dec6ccecd95269d40465
Arnaud Lachaume <arnaud.lachaume@maestrano.com>
2014-11-06 20:16:51 +1100

    added model placeholder

--- src/main/java/com/maestrano/connec/CnNote.java
+++ src/main/java/com/maestrano/connec/CnNote.java
@@ -1,5 +1,28 @@
 package com.maestrano.connec;
 
-public class CnNote {
-
+public class CnNote extends CnBaseObject {
+	private String tag;
+	private String description;
+	private String value;
+	
+	public String getTag() {
+		return tag;
+	}
+	public void setTag(String tag) {
+		this.tag = tag;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getValue() {
+		return value;
+	}
+	public void setValue(String value) {
+		this.value = value;
+	}
+	
+	
 }
