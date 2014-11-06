48b6304259c3d823c5db4ae2d6e95abad5e10159
Arnaud Lachaume <arnaud.lachaume@maestrano.com>
2014-11-06 20:17:10 +1100

    added Company model

---
+++ src/main/java/com/maestrano/connec/CnCompany.java
@@ -0,0 +1,98 @@
+package com.maestrano.connec;
+
+import java.util.TimeZone;
+
+public class CnCompany {
+	private String name;
+	private String currency;
+	private String note;
+	private TimeZone timezone;
+	private Integer capital;
+	private String juridicalStatus;
+	private String vatNumber;
+	private String fiscalYearFirstMonth;
+	private CnEmail email;
+	private CnAddressGroup address;
+	private CnWebsite website;
+	private CnPhone phone;
+	private String logoUrl;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCurrency() {
+		return currency;
+	}
+	public void setCurrency(String currency) {
+		this.currency = currency;
+	}
+	public String getNote() {
+		return note;
+	}
+	public void setNote(String note) {
+		this.note = note;
+	}
+	public TimeZone getTimezone() {
+		return timezone;
+	}
+	public void setTimezone(TimeZone timezone) {
+		this.timezone = timezone;
+	}
+	public Integer getCapital() {
+		return capital;
+	}
+	public void setCapital(Integer capital) {
+		this.capital = capital;
+	}
+	public String getJuridicalStatus() {
+		return juridicalStatus;
+	}
+	public void setJuridicalStatus(String juridicalStatus) {
+		this.juridicalStatus = juridicalStatus;
+	}
+	public String getVatNumber() {
+		return vatNumber;
+	}
+	public void setVatNumber(String vatNumber) {
+		this.vatNumber = vatNumber;
+	}
+	public String getFiscalYearFirstMonth() {
+		return fiscalYearFirstMonth;
+	}
+	public void setFiscalYearFirstMonth(String fiscalYearFirstMonth) {
+		this.fiscalYearFirstMonth = fiscalYearFirstMonth;
+	}
+	public CnEmail getEmail() {
+		return email;
+	}
+	public void setEmail(CnEmail email) {
+		this.email = email;
+	}
+	public CnAddressGroup getAddress() {
+		return address;
+	}
+	public void setAddress(CnAddressGroup address) {
+		this.address = address;
+	}
+	public CnWebsite getWebsite() {
+		return website;
+	}
+	public void setWebsite(CnWebsite website) {
+		this.website = website;
+	}
+	public CnPhone getPhone() {
+		return phone;
+	}
+	public void setPhone(CnPhone phone) {
+		this.phone = phone;
+	}
+	public String getLogoUrl() {
+		return logoUrl;
+	}
+	public void setLogoUrl(String logoUrl) {
+		this.logoUrl = logoUrl;
+	}
+}
