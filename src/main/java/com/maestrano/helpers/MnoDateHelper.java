2704e87dd90d90c53094db021a127e82962e120a
x4d3 <git@xade.eu>
2017-06-30 11:12:40 +0100

    [MNO-662] Issue with Java SDK
    - Fixe
    - Added jadler for mocktesting of http request
    - Added joda-time for iso8601 Parsing ("2016-08-01T15:30:20.000Z" could
    not be parsed)

--- src/main/java/com/maestrano/helpers/MnoDateHelper.java
+++ src/main/java/com/maestrano/helpers/MnoDateHelper.java
@@ -1,25 +1,20 @@
 package com.maestrano.helpers;
 
 import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.TimeZone;
+
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeZone;
 
 public class MnoDateHelper {
-	private static final ThreadLocal<SimpleDateFormat> DATE_FORMAT = new ThreadLocal<SimpleDateFormat>() {
-		protected SimpleDateFormat initialValue() {
-			SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ");
-			dateFormat.setTimeZone(TimeZone.getTimeZone("UTC"));
-			return dateFormat;
-		}
-	};
 
 	public static Date fromIso8601(String dateStr) throws ParseException {
-		return DATE_FORMAT.get().parse(dateStr.replaceAll("Z$", "+0000"));
+		return DateTime.parse(dateStr).toDate();
 	}
 
 	public static String toIso8601(Date dateObj) {
-		return DATE_FORMAT.get().format(dateObj).replaceAll("\\+0000$","Z");
-
+		// Joda Time toString is by default Iso 8601
+		// http://joda-time.sourceforge.net/apidocs/org/joda/time/base/AbstractInstant.html#toString%28%29
+		return new DateTime(dateObj, DateTimeZone.UTC).toString();
 	}
 }
